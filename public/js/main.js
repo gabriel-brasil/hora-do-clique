bbe9702357cefa898dbd1fdc4a7c2766bd7e4e4c
Gabriel <gabrielbrasil.dev@gmail.com>
2020-11-07 11:31:56 -0300

    Criação da estrutura base

---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+const btn = document.querySelector('#btn')
+const time = document.querySelector('#time')
+
+const today = new Date()
+
+// criaro objeto de horários
+const days = {
+  segunda: {
+    entrada: 0,
+    saidaAlmoco: 0,
+    voltaAlmoco: 0,
+    saida: 0
+  },
+}
+
+// adicionar ao storage o objeto de horários
+if (!localStorage.getItem("Dias")) {
+  localStorage.setItem("Dias", JSON.stringify(days))
+}
+
+// prencher o html com os campos salvos
+function fillFields() {
+  let currentStorage = JSON.parse(localStorage.getItem("Dias"))
+  time.textContent = ''
+  time.append(currentStorage.segunda.entrada)
+}
+
+// salvar o horário no local storage
+function addTime() {
+  const today = new Date()
+  let hour = today.getHours()
+  let minute = today.getMinutes()
+
+  if (hour < 10) { hour = '0' + hour }
+  if (minute < 10) { minute = '0' + minute }
+
+  const finalTime = hour + ":" + minute
+
+  let currentStorage = JSON.parse(localStorage.getItem("Dias"))
+
+  currentStorage.segunda.entrada = finalTime
+  localStorage.setItem("Dias", JSON.stringify(currentStorage))
+
+  fillFields()
+}
